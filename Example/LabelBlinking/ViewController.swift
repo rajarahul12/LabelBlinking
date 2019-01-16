fa9cf2f49d7b75d3f822564854579f6bc72bebdd
Raja Rahul <rajarahul@Rajas-MacBook-Pro.local>
2019-01-16 10:25:44 +0530

    Initial commit

---
+++ Example/LabelBlinking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LabelBlinking
+//
+//  Created by rajarahul12 on 01/16/2019.
+//  Copyright (c) 2019 rajarahul12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
